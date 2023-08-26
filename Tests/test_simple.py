8ff9aceb886fe522a71a75e15b3f39f4612c98e8
eytanohana <eohana98@gmail.com>
2023-08-26 11:58:55 +0300

    add tests

---
+++ Tests/test_simple.py
@@ -0,0 +1,20 @@
+
+
+def test_plus():
+    x = 5 + 4
+    assert x == 9
+
+
+def test_minus():
+    x = 5 - 4
+    assert x == 1
+
+
+def test_times():
+    x = 5 * 4
+    assert x == 20
+
+
+def test_divide():
+    x = 20 / 5
+    assert x == 4
