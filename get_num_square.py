c823da060f86b3616a84cf88eab1c9e3ea1fc8c1
eytanohana <eohana98@gmail.com>
2025-12-21 01:25:28 +0200

    write to github output

--- get_num_square.py
+++ get_num_square.py
@@ -1,6 +1,9 @@
 # src/get_num_square.py
 import os
 
+
+output_path = os.environ["GITHUB_OUTPUT"]
+
 # get the input and convert it to int
 num = os.environ.get("INPUT_NUM")
 if num:
@@ -12,4 +15,6 @@ else:
     num = 1
 
 # to set output, print to shell in following syntax
-print(f"::set-output name=num_squared::{num ** 2}")
+# print(f"::set-output name=num_squared::{num ** 2}")
+with open(output_path, 'a') as f:
+    f.write(F'num_squared={num ** 2}')
